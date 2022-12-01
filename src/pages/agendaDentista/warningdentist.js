4b20b5ecdd97f7913601d69ea57e348921a555b3
Gleyci-Macena <gleycianemc@gmail.com>
2022-12-01 04:16:23 -0300

    agendamentos e pendentes css

--- src/pages/agendaDentista/warningdentist.js
+++ src/pages/agendaDentista/warningdentist.js
@@ -4,18 +4,7 @@ export default () => {
   const template = `
     <div class='patient-body'>
       <div id='form-options'>
-        <img class='icon-calendar' src='./icon/icons8-calendar-64 (2).png' alt='icon calendar'>
-        <p class='text'> Marque sua consulta 
-        <br> ou veja seus agendamentos: </p>
-        <button id='btScheduling'>Agendamentos</button>
-        <select name='rede-credenciada' id='select-grid'>
-          <option value> Rede credenciada </option>
-          <option value='Centro-Oeste'>Centro-Oeste</option>
-          <option value='Nordeste'>Nordeste</option>
-          <option value='Norte'>Norte</option>
-          <option value='Sudeste'>Sudeste</option>
-          <option value='Sul'>Sul</option>
-        </select>
+        <button id='btScheduling' class='btSchedulingw'>Consultas Pendentes</button>
       </div>
       <section class='containerDentists'></section>
     </div>
@@ -42,16 +31,17 @@ export default () => {
                   containerDentist.innerHTML = scheduling.map((item) => {  
                   return `       
                   <div class='warning-body'>
-                  <section class= 'container-warning'>
-                    <div class='infos'>
-                      <p class='txt-status'>Status da consulta:</p>
+                  <section class= 'dentist-warning-w'>
+                    <div class='dentist-info'>
+                      <p class='txt-dentist'>Status da consulta:</p>
                       <div class='ipt-situation'>${item.Status}</div>
-                      <p class='txt-status'>Data do agendamento: ${item.Date}</p>
-                      <p class='txt-status'>Data do agendamento: ${item.Patient}</p>
-                      <p class='warning'>Qualquer dúvida entre em contato <br>pelo site através do <a class='link' href='https://beneficiario.odontoprev.com.br/fale-conosco'>fale conosco.</a></p>
-                    </div>
-                    <div class='confirmation'>
-                     <button class='btn-back'>Voltar</button>
+                      <p class='txt-dentist'>Nome do beneficiário: ${item.Patient}</p>
+                      <p class='txt-dentist'>Data do agendamento: ${item.Date}</p>
+                      <p class='warning'>Qualquer informação entre em contato com <br> o beneficiário através do e-mail:<br> ${item.emailPatient} .</p>
+                      <div class='btns'>
+                        <button id='btnAcept' class='btnAcept'>Aceitar</button>
+                        <button id='btnDeny' class='btnDeny'>Recusar</button>
+                      </div>
                     </div>
                   </section>
                 </div>    
